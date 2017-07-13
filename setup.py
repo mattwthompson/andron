167a67ae9c3ce4491ed1b0ef2fd421bcb46cdd7a
matt.thompson@vanderbilt.edu <matt.thompson@vanderbilt.edu>
2017-07-13 14:35:09 -0500

    Update package information

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='andron',
+      version='0.0',
+      description='Assign charge to molecules',
+      url='http://github.com/mattwthompson/andron',
+      author='Matthew W. Thompson',
+      author_email='matt.thompson@vanderbilt.edu',
+      license='MIT',
+      packages=['andron'],
+      zip_safe=False)
